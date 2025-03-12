9230de23862c8d794b002f4c5763d0a02c861c27
L-Salvo <salvoschaferlautaro@gmail.com>
2025-03-12 16:23:44 -0300

    Comentarios sobre anemia en las clases

--- src/main/java/ar/edu/unrn/tp0/objetos/anemicos/Main.java
+++ src/main/java/ar/edu/unrn/tp0/objetos/anemicos/Main.java
@@ -12,11 +12,24 @@ public class Main {
         //Imprimir TiempoAnemico
         System.out.println(tiempoAnemico.getTiempo().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
         System.out.println(tiempoAnemico.getTiempo().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        /*
+           La clase TiempoAnemico es anemica debido a que no cuenta con comportamiento propio,
+           su funcion es solo almacenar un tiempo determinado y unicamente cuenta con un metodo
+           que sirven para obtener dicho tiempo.
+           En este caso se deseaba imprimir en consola el tiempo en formato largo y formato corto, y debido a que
+           TiempoAnemico no cuenta con dicho comportamiento, la logica fue implementada fuera de la clase.
+        */
+
 
         //Imprimir TiempoNoAnemico
         tiempoNoAnemico.imprimirFormatoLargo();
         tiempoNoAnemico.imprimirFormatoCorto();
-
+        /*
+           La clase TiempoNoAnemico no es anemica debido a que cuenta con comportamiento propio, es decir,
+           cuenta con metodos capaces de realizar la tarea que planteamos anteriormente.
+           Gracias a esto la logica esta encapsulada en la clase y podemos delegarle la tarea al objeto
+           simplemente utilizando la firma del metodo. Esto a su vez genera un desacoplamiento entre clases
+        */
 
     }
 }
